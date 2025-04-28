a460c81a3f49daaea857aa2dc6d707111e5226a1
Shashank S <shashank5418shashu@gmail.com>
2025-04-28 19:45:15 +0530

    Card details

--- frontend/rakshamitra/src/components/Servicess/Card.jsx
+++ frontend/rakshamitra/src/components/Servicess/Card.jsx
@@ -6,13 +6,16 @@ const Card = () => {
     <div className='card'>
 
         <h1>Police</h1>
+        <div className='imagecenter'>
         <img src="https://png.pngtree.com/png-vector/20240910/ourmid/pngtree-policeman-avatar-png-image_13805775.png" alt="" />
-
         <ul>
-            <li>Money Fraud</li>
-            <li>Annonyums Tip</li>
-            <li>Human Safety</li>
+            <li><button className="carddetails" >Fraud</button></li>
+            <li><button className="carddetails" >Tip</button></li>
+            <li><button className="carddetails" >Human Safety</button></li>
         </ul>
+
+        </div>
+
 
       
     </div>
